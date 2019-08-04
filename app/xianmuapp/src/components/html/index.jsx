3d9ccf9bd8b94cbacc20cac6fbe999d64856f568
leixiaopengxiha <leixiaopengxiha@163.com>
2019-08-04 23:59:59 +0800

    sd

--- app/xianmuapp/src/components/html/index.jsx
+++ app/xianmuapp/src/components/html/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { getLogin} from '../api/index'
 import './html.scss'
 import  Tankuang from  '../tankuang'
 import Login from '../login'
@@ -11,9 +10,6 @@ export default class index extends Component {
             user:'lxp123',
             password:'123456'
         }
-        getLogin(fromData).then(res=>{
-            console.log(res)
-        })
     }
     render() {
         return (
