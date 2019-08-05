1769a293d0fc1825c695e6774c35fc452876895e
xixin <2793030875@qq.com>
2019-08-05 11:04:43 +0800

    'home'

--- app/xianmuapp/src/components/html/index.jsx
+++ app/xianmuapp/src/components/html/index.jsx
@@ -1,19 +1,15 @@
 import React, { Component } from 'react'
 import './html.scss'
-import  Tankuang from  '../tankuang'
+import Tankuang from '../tankuang'
 import Login from '../login'
 import Home from '../home/index'
-<<<<<<< HEAD
-=======
-
->>>>>>> b82a14b669bdba68dc359c537860264a5b50d8e8
 import { Switch, Route } from 'react-router-dom'
 
 export default class index extends Component {
-    componentDidMount(){
-        const fromData={
-            user:'lxp123',
-            password:'123456'
+    componentDidMount() {
+        const fromData = {
+            user: 'lxp123',
+            password: '123456'
         }
     }
     render() {
@@ -21,7 +17,7 @@ export default class index extends Component {
             <div className="html-box">
                 <Switch>
                     <Route path='/' exact component={Tankuang} />
-                    <Route path='/login'  component={Login} />
+                    <Route path='/login' component={Login} />
                     <Route path='/home' component={Home} />
                 </Switch>
             </div>
