6660f66636fff44b91aa6b1dee1f3f9837946795
leixiaopengxiha <leixiaopengxiha@163.com>
2019-08-05 10:41:25 +0800

    sda

--- app/xianmuapp/src/components/home/index.jsx
+++ app/xianmuapp/src/components/home/index.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
-import Headers from './headers'
-
+import Header from './header'
+import Images from './img'
+import Linke from '../likepages'
 import Publish from '../publishpage'
-
 import './home.scss'
-import { Route, Switch} from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 export default class Html extends Component {
     render() {
         return (
             <div>
+                <div className=''>
+                    <Header></Header>
+                </div>
                 <main>
                     <Switch>
-                         <Route path='/home/publish' component={Publish} />
-                         <Route path='/home'  component={Headers} />
+                        <Route path='/home/lofter' component={() => <Images></Images>} />
+                        <Route path='/home/liked' component={() => <Linke></Linke>} />
+                        <Route path='/home/publish' component={Publish} />
+                        <Redirect to='/home/lofter' />
                     </Switch>
                 </main>
             </div>
